4a516f1b9a0bba79b35542ca0d797cb957c2ff99
Emile Fokkema <emilefokkema@gmail.com>
2018-11-20 16:49:53 +0100

    make test pass by checking that the property reference is to a local, not a variable of the lambda

--- HttpClientAnalyzer/Analyzer1/Analyzer1/LazyLoadingPropertyAnalyzer.cs
+++ HttpClientAnalyzer/Analyzer1/Analyzer1/LazyLoadingPropertyAnalyzer.cs
@@ -65,7 +65,7 @@ namespace Analyzer1
 
         private static void AnalyzePropertyReference(IPropertyReferenceOperation operation, OperationAnalysisContext context, string methodName)
         {
-            if (operation.Property.IsVirtual)
+            if (operation.Property.IsVirtual && operation.Instance.Kind == OperationKind.LocalReference)
             {
                 var diagnostic = Diagnostic.Create(LazyEvaluationInExpressionRule, operation.Syntax.GetLocation(), operation.Property.Name,
                     methodName);
